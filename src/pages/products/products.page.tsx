a84c92e7a6331ed7cf40f863e8aaee72360e3bde
Yeshu Kochher <yeshu@outlook.in>
2025-04-19 12:33:59 +0530

    feat: fetching products and displaying them

--- src/pages/products/products.page.tsx
+++ src/pages/products/products.page.tsx
@@ -1,8 +1,30 @@
+import { gql, useQuery } from "@apollo/client";
+
+const GET_PRODUCTS = gql`
+  query GetProducts {
+    getProducts {
+      id
+      title
+      price
+    }
+  }
+`;
+
 const ProductsPage = () => {
+  const { loading, data } = useQuery(GET_PRODUCTS)
+
+  if (loading) return <p>Loading...</p>;
+
   return (
     <div>
       <h1>Products</h1>
       <p>List of products will be displayed here.</p>
+      {data.getProducts.map((p) => (
+        <div key={p.id}>
+          <h2>{p.title}</h2>
+          <p>Price: ${p.price}</p>
+        </div>
+      ))}
     </div>
   );
 };
